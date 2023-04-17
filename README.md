728828b881f6c4cc806b0f374e333130042f4c7b
nagy nabil <nagynabil651@gmail.com>
2023-04-17 05:38:08 +0200

    featS: BIG THINGS HERE IN DAY ONE - moked marks store as array - the ability to add to the marks but with limitions - ui (quick pick) to display marked files - jump between marks - hard coded 3 commands for first 3 items in the array

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 This is the README for your extension "booker". After writing up a brief description, we recommend including the following sections.
 
+## TODOS
+
+- [ ] delete mark
+- [ ] ORDER MARKS
+- [ ] figure out when to change saved position for the mark
+- [ ] figure out how to use vscode datastorge to store information for the extension per workspace
+- [ ] search can we make generic commands like for example send index with the command(deal with the command as function)
+
 ## Features
 
 Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
@@ -24,8 +32,8 @@ For example:
 
 This extension contributes the following settings:
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+- `myExtension.enable`: Enable/disable this extension.
+- `myExtension.thing`: Set to `blah` to do something.
 
 ## Known Issues
 
@@ -53,19 +61,19 @@ Added features X, Y, and Z.
 
 Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
 
-* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+- [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
 
 ## Working with Markdown
 
 You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
+- Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
+- Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
 
 ## For more information
 
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,211 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+
+type MarksStoreItem = [vscode.Uri, vscode.Position];
+type MarksStore = (MarksStoreItem | null)[];
+let globalSavedPos: vscode.Position;
+let globalSavedFileUri: vscode.Uri;
+// TODO make the store in some better place? data storge?
+const flags = new Set<string>();
+let freeIndex = 0;
+const marks: MarksStore = new Array(3).fill(null);
+
+function insertMark(store: MarksStore, mark: MarksStoreItem) {
+    if (freeIndex >= 3 || flags.has(mark[0].toString())) {
+        console.log("didnt saved new items someting against me idk");
+        // already stored 3 marks don't store any new
+        // mark already exist dont add it again
+        return;
+    }
+    flags.add(mark[0].toString());
+    store[freeIndex++] = mark;
+}
+
+// function removeMark(store: MarksStore, markIndex: number) {
+//     // TODO store length is hard coded fix this shit
+//     if (markIndex >= 3) {
+//         // cannot remove something i don't have
+//         return;
+//     }
+//     store[markIndex] = null;
+//     // TODO how to integrate this with freeIndex and the holes you would leave in the array
+// }
+
+function showAllMarks(marks: MarksStore) {
+    const options = marks
+        .filter((v) => {
+            return v === null ? false : true;
+        })
+        .map((v, i) => {
+            return (v as MarksStoreItem)[0].path + " - " + i.toString();
+        });
+    console.log(marks);
+    console.log(options);
+    vscode.window
+        .showQuickPick(options, {
+            canPickMany: false,
+            title: "booker-marks",
+        })
+        .then((selection) => {
+            if (selection !== undefined) {
+                const parsedIndex = +selection[selection.length - 1];
+                console.log(`user choosed to go to ${selection}`);
+                console.log(`index ${parsedIndex}`);
+                jumpToMark(marks, parsedIndex);
+            }
+        });
+}
+
+async function jumpToMark(store: MarksStore, markIndex: number) {
+    // load and open the saved file
+    // TODO jump to correct position
+    if (store[markIndex] === null && markIndex >= store.length) {
+        // didn't store mark in this index do shit
+        // out of index do shit
+        return;
+        // throw new Error("trying to access out of index");
+    }
+    const doc = await vscode.workspace.openTextDocument(
+        (store[markIndex] as MarksStoreItem)[0]
+    );
+    vscode.window.showTextDocument(doc, {});
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "booker" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "booker" is now active!');
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand(
+        "booker.mark.append",
+        () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("Hello World from booker!");
+            const textEditor = vscode.window.activeTextEditor;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('booker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from booker!');
-	});
+            // how to get the current position if no text editor so leave if no one
+            if (textEditor === undefined) {
+                return;
+            }
+            const doc = textEditor.document;
+            // store uri globallay
+            globalSavedFileUri = doc.uri;
 
-	context.subscriptions.push(disposable);
+            const position = textEditor.selection.active;
+            // store pos globally
+            globalSavedPos = position;
+
+            //save in the store
+            insertMark(marks, [doc.uri, position]);
+
+            console.log(
+                `i saved file ${globalSavedFileUri.fsPath} at line at ${globalSavedPos.line}`
+            );
+
+            // vscode.workspace.onWillSaveTextDocument((e) => {
+            //     console.log(`file ${e.document.uri} saved`);
+            //     if (
+            //         e.document.uri.toString() === globalSavedFileUri.toString()
+            //     ) {
+            //         console.log("saved the global");
+            //     }
+            // });
+
+            // vscode.workspace.onDidCloseTextDocument
+
+            //
+            // vscode.window.onDidChangeActiveTextEditor((e) => {
+            //     if (e === undefined) {
+            //         return;
+            //     }
+
+            //     console.log(`went to file ${e.document.uri}`);
+            //     if (
+            //         e.document.uri.toString() !== globalSavedFileUri.toString()
+            //     ) {
+            //         console.log("it's not the same as global one");
+            //         console.log(
+            //             `doc last pos before moving to another file is ${textEditor.selection.active}`
+            //         );
+            //     }
+            // });
+        }
+    );
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable2 = vscode.commands.registerCommand(
+        "booker.mark.jump",
+        async () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("jumping?");
+            console.log(
+                `i must jump to ${globalSavedFileUri.path} to line ${globalSavedPos.line}`
+            );
+        }
+    );
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable3 = vscode.commands.registerCommand(
+        "booker.marks.show",
+        async () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("show all marks?");
+            showAllMarks(marks);
+        }
+    );
+
+    // just as day one i will create hard coded commands to go to certain index in the store 0 -> 2 [1->3]
+    let goto0 = vscode.commands.registerCommand(
+        "booker.mark.jump-fir",
+        async () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("goto mark number 1");
+            await jumpToMark(marks, 0);
+        }
+    );
+
+    // just as day one i will create hard coded commands to go to certain index in the store 0 -> 2 [1->3]
+    let goto1 = vscode.commands.registerCommand(
+        "booker.mark.jump-sec",
+        async () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("goto mark number 2");
+            await jumpToMark(marks, 1);
+        }
+    );
+
+    // just as day one i will create hard coded commands to go to certain index in the store 0 -> 2 [1->3]
+    let goto2 = vscode.commands.registerCommand(
+        "booker.mark.jump-thrd",
+        async () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage("goto mark number 1");
+            await jumpToMark(marks, 2);
+        }
+    );
+
+    context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable2);
+    context.subscriptions.push(disposable3);
+    context.subscriptions.push(goto0);
+    context.subscriptions.push(goto1);
+    context.subscriptions.push(goto2);
 }
 
 // This method is called when your extension is deactivated
